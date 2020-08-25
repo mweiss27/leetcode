9253126851bff3f35c2ab3e62eac228c68c97ee6
Matt Weiss <mattweiss37@gmail.com>
2020-08-25 12:56:55 -0400

    Multiply with inline addition

--- 43.MultiplyStrings/index.js
+++ 43.MultiplyStrings/index.js
@@ -4,54 +4,24 @@
  * @return {string}
  */
 var multiply = function (num1, num2) {
-  const products = []
-
+  const products = Array(num1.length + num2.length).fill(0)
   for (let i = num1.length - 1; i >= 0; i--) {
-    for (let j = num2.length - 1; j >= 0; j--) {
-      let placesShift = num1.length - i + (num2.length - j) - 2
+    let carry = 0
 
-      const result = +num1[i] * +num2[j] * Math.pow(10, placesShift)
-      if (result > 0) {
-        products.push(`${result}`)
-      }
+    for (let j = num2.length - 1; j >= 0; j--) {
+      products[i + j + 1] += num1[i] * num2[j] + carry
+      carry = Math.floor(products[i + j + 1] / 10)
+      products[i + j + 1] = products[i + j + 1] % 10
     }
+    products[i] += carry
   }
 
-  products.sort((a, b) => a - b)
-  console.log(products.join(','))
-  return products.reduce((result, current) => {
-    // console.log(`Adding '${current}' to our current result of '${result}'`)
-    if (!result) {
-      return `${current}`
-    }
-
-    let nextResult = ""
-    let i = result.length - 1
-    let j = current.length - 1
-    let carry = 0
-    while (i >= 0 || j >= 0) {
-      const rNum = result[i--] || 0
-      const cNum = current[j--] || 0
-      let added = +rNum + +cNum + carry
-      carry = 0
-      if (added >= 10) {
-        carry = 1
-        added -= 10
-      }
-
-      nextResult = `${added}${nextResult}`
-    }
-    if (carry > 0) {
-      nextResult = `${carry}${nextResult}`
-    }
-
-    return nextResult
-  }, "")
+  return products.join("").replace(/^0*(\d)/, "$1")
 }
 
 // console.log(multiply("123456789", "987654321"), "121932631112635269")
-console.log(multiply("498828660196", "840477629533"), "419254329864656431168468")
-// console.log(multiply("123", "123"))
+// console.log(multiply("498828660196", "840477629533"), "419254329864656431168468")
+console.log(multiply("0", "0"))
 
 /* 
 123
