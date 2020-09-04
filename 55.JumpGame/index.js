f599986ade80094f93b87a1ae6a0d82895a7286e
Matt Weiss <matt.weiss@igs.com>
2020-09-04 16:06:31 -0400

    jump game

---
+++ 55.JumpGame/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let lastValidIndex = nums.length - 1
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= lastValidIndex) {
+        lastValidIndex = i
+    }
+  }
+
+  return lastValidIndex === 0
+}
+
+console.log(canJump([2, 3, 1, 1, 4]))
+console.log(canJump([3, 2, 1, 0, 4]))
+console.log(canJump([2, 0]))
